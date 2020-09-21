346ac5bf52d63f5792a5e30f60bf0ddcfc335025
edsaac <esaavedrac@u.northwestern.edu>
2020-09-21 11:59:31 -0500

    Add longitudinal dispersion + Pretier curves

--- test/README.md
+++ test/README.md
@@ -1,7 +1,21 @@
 # Results from tests
 
-- Plug flow
-- Only longitudinal dispersion
-- Only attachment (sink)
-- Attachment & detachment (source + sink)
-- Attachment/detachment & decay
+## Plug flow
+
+![plugFlow](./plugFlow/breakthrough.png)
+
+## Only longitudinal dispersion
+
+![plugFlow](./longitudinalDispersion/breakthrough.png)
+
+## Only attachment (sink)
+
+![plugFlow](./onlyAttachment/breakthrough.png)
+
+## Attachment & detachment (source + sink)
+
+![plugFlow](./attachmentDetachment/breakthrough.png)
+
+## Attachment, detachment & decay
+
+![plugFlow](./allProcesses/breakthrough.png)
--- tools/organizeResults.py
+++ tools/organizeResults.py
@@ -30,12 +30,16 @@ Legend=["$\\dfrac{[V_{(aq)}]}{[V_{(aq)}]_0}$"]
 fig = plt.figure(figsize=(5,4),facecolor="white")
 ax1 = plt.subplot(1,1,1)
 ax1.plot(Time,Cnorm,c="violet",lw=3,label=Legend[0])
-ax1.set_ylim([1.0E-3,1.2])
+
+ax1.set_yscale("symlog",\
+	linthreshy=1.0E-4,subsy=[1,2,3,4,5,6,7,8,9])
+ax1.set_ylim([-1.0E-5,1.15])
 ax1.set_xlim([0,10])
 
-ax1.set_yscale("log")
+#ax1.yaxis.grid(True, which='minor')
+#ax1.yaxis.set_minor_locator(MultipleLocator(5))
 
 #ax1.set_ylabel(Legend[0],fontsize="large",rotation=0)
 ax1.set_xlabel("Time [$a$]",fontsize="large")
 ax1.legend(fontsize="large")
-plt.savefig("./breaktrough.png",transparent=True)
+plt.savefig("./breakthrough.png",transparent=True)
