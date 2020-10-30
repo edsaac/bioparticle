6f830cfc57d6b25a8d53d4d0992936dc4594c15b
edsaac <esaavedrac@u.northwestern.edu>
2020-10-30 13:28:10 -0500

    fixed overflow of processors

--- doeTests/3DCube/miscellaneous/handlePIDS.sh
+++ doeTests/3DCube/miscellaneous/handlePIDS.sh
@@ -2,9 +2,12 @@ PFLOTRAN_path="$PFLOTRAN_DIR/src/pflotran/pflotran"
 LIST=$(ls CASE*/*.in)
 N=10
 
+task(){
+  local run=$1
+}
+
 for d in $LIST ; do
    ((i=i%N)); ((i++==0)) && wait
-   echo "$PFLOTRAN_path -pflotranin $d"
-   $PFLOTRAN_path -pflotranin $d &
+   task "$PFLOTRAN_path -pflotranin $d" &
 done
 
