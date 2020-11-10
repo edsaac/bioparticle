aff0db705ea1f26ca7df5acf2336077d40cece66
edsaac <esaavedrac@u.northwestern.edu>
2020-11-10 16:28:28 -0600

    add GNUparallel !

--- doeTests/3DCube/miscellaneous/handlePIDS.sh
+++ doeTests/3DCube/miscellaneous/handlePIDS.sh
@@ -2,12 +2,4 @@ PFLOTRAN_path="$PFLOTRAN_DIR/src/pflotran/pflotran"
 LIST=$(ls CASE*/*.in)
 N=10
 
-task(){
-  local run=$1
-}
-
-for d in $LIST ; do
-   ((i=i%N)); ((i++==0)) && wait
-   task "$PFLOTRAN_path -pflotranin $d" &
-done
-
+parallel --jobs $N $PFLOTRAN_path -pflotranin ::: $LIST
